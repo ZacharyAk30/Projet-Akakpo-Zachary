6ac0f2097dde1e5547ccf1e3b48d6c74b191cd43
ZacharyAk30 <96988490+ZacharyAk30@users.noreply.github.com>
2022-12-09 12:05:28 +0100

    nsdfkl

--- Layoute/page.py
+++ Layoute/page.py
@@ -5,30 +5,18 @@ import datetime
 
 def pageLayoute():
     navbar=option_menu(
-            menu_title="Pages",
+            menu_title="",
             options=["Caracteristique","Map","Meilleurs Ventes","Analyse sur un departement"],
             icons=["house","book","book","book"],
             menu_icon="cast",
             default_index=0,
             orientation="horizontal",
-            styles={
-                "container": {"padding": "0!important", "background-color": "black"},
-                "icon": {"color": "orange", "font-size": "25px"}, 
-                "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-                "nav-link-selected": {"background-color": "gray"},
-            }
         )
     with st.sidebar:
         selectedata=option_menu(
                 menu_title="Dataframe",
                 options=["2016","2017","2018","2019","2020"],
                 default_index=0,
-                styles={
-                    "container": {"padding": "0!important", "background-color": "black"},
-                    "icon": {"color": "orange", "font-size": "25px"}, 
-                    "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-                    "nav-link-selected": {"background-color": "gray"},
-                }
             )
         st.title("Date")
         start_date = st.date_input(
